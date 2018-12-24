2e16cb958c09b24516475eb7c71f4d71f189fb71
guoyjalihy <guoyjalihy@icloud.com>
2018-12-24 15:02:11 +0800

    first commit

---
+++ src/main/java/com/common/portal/PortalApplication.java
@@ -0,0 +1,14 @@
+package com.common.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalApplication.class, args);
+	}
+
+}
+
